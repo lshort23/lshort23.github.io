89c9de40d24f01e24592ae842513322194c33852
lshort23 <lshort23@byui.edu>
2019-03-18 20:02:50 -0600

    wind chill

--- assignments/lesson-10/script/windchill.js
+++ assignments/lesson-10/script/windchill.js
@@ -5,5 +5,6 @@ var s = parseInt(document.getElementById('wind').innerHTML);
 
 var f = 35.74 + (0.6215*t) - (35.75 * Math.pow(s, 0.16)) + 0.4275 * t * (Math.pow(s, 0.16));
 
+
 document.getElementById('output').innerHTML = f;
 }
