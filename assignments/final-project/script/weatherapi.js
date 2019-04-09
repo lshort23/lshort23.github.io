d2ec1bcdea251038e78a7547b13d9eaa49cf981a
lshort23 <lshort23@byui.edu>
2019-04-08 18:32:49 -0600

    the beginning

---
+++ assignments/final-project/script/weatherapi.js
@@ -0,0 +1,15 @@
+let weatherRequest = new XMLHttpRequest();
+
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=42ad6f966f5df0f08d1f672debefa31e';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+document.getElementById('temp').innerHTML = weatherData.main.temp;
+document.getElementById('current').innerHTML = weatherData.main.temp;
+document.getElementById('humid').innerHTML = weatherData.main.humidity;
+document.getElementById('wind').innerHTML = weatherData.wind.speed;
+}
